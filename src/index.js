71399738579e05b2c4d7b4244f75bcafd1fac944
Seren Kaya <srnky62@gmail.com>
2024-07-25 21:17:18 +0300

    Loops with v-for

--- src/index.js
+++ src/index.js
@@ -6,25 +6,34 @@ const app = Vue.createApp({
   template: `
       <button v-on:click="increment()"> Increment </button>
       <p> {{count}}</p>
-      <div v-if="isEven()"> EVEN </div>
-      <div v-else="isOdd()"> Odd </div>
+      <div v-for="number in numbers">
+        <div>
+            {{number}} 
+            <span v-if="isEven(number)"> Even </span>
+            <span v-else="isOdd(number)"> Odd </span>
+        </div>
+      </div>
+      <p>
+      <div v-if="isEven(count)"> EVEN </div>
+      <div v-else="isOdd(count)"> Odd </div>
 
     `,
 
   data() {
     return {
       count: 0,
+      numbers: [1, 2, 3, 4, 5],
     };
   },
   methods: {
     increment() {
       this.count += 1;
     },
-    isEven() {
-      return this.count % 2 == 0;
+    isEven(val) {
+      return val % 2 == 0;
     },
-    isOdd() {
-      return this.count % 2 != 0;
+    isOdd(val) {
+      return val % 2 != 0;
     },
   },
 });

e4cd1bcd9ce4205b4d920d6c7a7f5dca646f58f6
Seren Kaya <srnky62@gmail.com>
2024-07-25 21:23:14 +0300

    pull v-7 branch

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
 import * as Vue from "vue/dist/vue.esm-bundler.js";
 
+//v-if
+//v-else
 const app = Vue.createApp({
   template: `
       <button v-on:click="increment()"> Increment </button>
       <p> {{count}}</p>
+      <div v-for="number in numbers">
+        <div>
+            {{number}} 
+            <span v-if="isEven(number)"> Even </span>
+            <span v-else="isOdd(number)"> Odd </span>
+        </div>
+      </div>
+      <p>
+      <div v-if="isEven(count)"> EVEN </div>
+      <div v-else="isOdd(count)"> Odd </div>
 
     `,
 
   data() {
     return {
       count: 0,
+      numbers: [1, 2, 3, 4, 5],
     };
   },
   methods: {
     increment() {
       this.count += 1;
+    },
+    isEven(val) {
+      return val % 2 == 0;
+    },
+    isOdd(val) {
+      return val % 2 != 0;
     },
   },
 });

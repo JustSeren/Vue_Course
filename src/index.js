b246f6218a090ac049c6d2a6665ff218cbc430ba
Seren Kaya <srnky62@gmail.com>
2024-07-25 20:55:46 +0300

    addind user interactions with methods

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import * as Vue from "vue/dist/vue.esm-bundler.js";
+
+const app = Vue.createApp({
+  template: `
+      <button v-on:click="increment()"> Increment </button>
+      <p> {{count}}</p>
+
+    `,
+
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count += 1;
+    },
+  },
+});
+app.mount("#app");

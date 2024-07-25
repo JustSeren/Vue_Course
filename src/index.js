1bfd1cb5da784f35350d26d342a0074c89db65c3
Seren Kaya <srnky62@gmail.com>
2024-07-25 23:51:26 +0300

    Child parent comminication with events

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import * as Vue from "vue/dist/vue.esm-bundler.js";
 const Num = {
   props: ["number"],
   template: `
-    <div  v-bind:class="getClass(number)">
+    <button v-bind:class="getClass(number)"
+     v-on:click="click"  >
     {{number}}
-    </div>
+  
+    </button>
+
     `,
   methods: {
+    click() {
+      this.$emit("chosen", this.number);
+    },
     getClass(number) {
       return this.isEven(number) ? "blue" : "red";
     },
@@ -23,34 +29,23 @@ const app = Vue.createApp({
     Num,
   },
   template: `
-      <button v-on:click="increment()"> Increment </button>
-      <p> {{count}}</p>
-      <hello greeting="Hello"/>
-       <hello greeting="Hi"/>
-      <input 
-        type = "checkbox"
-        v-model="value"
-        value="a"
-        />
-        <input 
-        type = "checkbox"
-        v-model="value"
-        value="b"
-        />
-        {{ value }}
-        <div class="red"> {{error}}</div>
-
+      
       <num
         v-for="number in numbers"
+       v-bind:number="number"
+       v-on:chosen="addNumber"
+      />
+        <hr/>
+      <num
+        v-for="number in numberHistory"
        v-bind:number="number"
       />
     `,
 
   data() {
     return {
-      count: 0,
       numbers: [1, 2, 3, 4, 5],
-      value: ["a", "b"],
+      numberHistory: [],
     };
   },
   computed: {
@@ -58,19 +53,10 @@ const app = Vue.createApp({
     evenList() {
       return this.numbers.filter((num) => this.isEven(num));
     },
-    error() {
-      if (this.value.length < 5) {
-        return "must be grater than 5";
-      }
-    },
   },
   methods: {
-    increment() {
-      this.count += 1;
-    },
-
-    isOdd(val) {
-      return val % 2 != 0;
+    addNumber(number) {
+      this.numberHistory.push(number);
     },
   },
 });

fd781eaeace36f22b21006d030fab2e622699756
Seren Kaya <srnky62@gmail.com>
2024-07-25 22:00:58 +0300

    create getClass method and use method in that method :)

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = Vue.createApp({
       <p> {{count}}</p>
       <div
         v-for="number in numbers"
-        v-bind:class="isEven(number) ?'blue' :'red'"
+        v-bind:class="getClass(number)"
       >
         <div>
             {{number}} 
@@ -29,6 +29,9 @@ const app = Vue.createApp({
     },
   },
   methods: {
+    getClass(number) {
+      return isEven(number) ? "blue" : "red";
+    },
     increment() {
       this.count += 1;
     },
